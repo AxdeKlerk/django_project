70f0e1fab21df2bd90530397bda6e856039dded8
GitHub AxdeKlerk <axdeklerk@gmail.com>
2025-04-21 17:48:41 +0100

    Fix Procfile with correct project name

--- my_project/urls.py
+++ my_project/urls.py
@@ -20,7 +20,7 @@ from hello_world import views as index_views
 from about import views as about_views
 
 urlpatterns = [
-    path('hello/', index_views.index, name='index'),  # This line maps the root URL to the index view in hello_world app
+    path('', index_views.index, name='index'),  # This line maps the root URL to the index view in hello_world app
     path('about/', about_views.about_me, name='about'),  # This line includes the URLs from the about app
     path('admin/', admin.site.urls),
 ]
